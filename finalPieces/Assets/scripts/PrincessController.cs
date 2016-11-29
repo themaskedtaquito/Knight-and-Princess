da3fee9d9bb6a527a921262a764e17227de419fd
shelbyfirebaugh <slf404@nyu.edu>
2016-11-29 14:42:49 -0500

    shelby-princess movement & shooting

---
+++ finalPieces/Assets/scripts/PrincessController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrincessController : MonoBehaviour {
+
+	public float speed;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+//	private Rigidbody2D rb2d;
+
+	void Start(){
+//		rb2d = GetComponent<Rigidbody2D> ();
+
+	}
+	void Update() {
+		float h = speed * Input.GetAxis("Mouse ScrollWheel");
+		transform.Translate(h, 0, 0);
+
+		if (Input.GetMouseButton (0) && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
+		
+
+//		float moveH = Input.GetAxis ("Mouse ScrollWheel");
+//
+//		Vector2 movement = new Vector2 (moveH, 0);
+//		rb2d.AddForce (movement * speed);
+	}
+}
---
+++ finalPieces/Assets/scripts/mover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mover : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	public float speed;
+
+	void Start(){
+		rb2d = GetComponent<Rigidbody2D> ();
+
+		rb2d.velocity = (transform.up * -1) * speed;
+	}
+}
