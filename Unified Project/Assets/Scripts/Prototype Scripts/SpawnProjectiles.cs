d4609ef3239cf2072047cd4327ca9563ce4440d1
LAPTOP-8PEB5H4U\minja <mta305@nyu.edu>
2016-11-30 08:32:14 -0500

    Both Players in the scene together
    I finally got github working guys. Now I guess we should work on level
    design and tweaking the speeds of the characters and collision

---
+++ Unified Project/Assets/Scripts/Prototype Scripts/SpawnProjectiles.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnProjectiles : MonoBehaviour {
+    public GameObject Slower;
+    public GameObject Knockdown;
+
+    private int interval;
+
+	// Use this for initialization
+	void Start () {
+        interval = Random.Range(2, 6);
+        InvokeRepeating("SpawnProjectile", 1, interval);
+	}
+	
+	// Update is called once per frame
+	void Update () {	    
+	}
+
+    void SpawnProjectile()
+    {
+        int SpawnChance = Random.Range(0, 2);
+        if (SpawnChance > 0)
+        {
+            int SpawnType = Random.Range(0, 2);
+            if (SpawnType > 0)
+            {
+                Instantiate(Knockdown, transform.position, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(Slower, transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Unified Project/Assets/Scripts/Tutorial Scrips/PlatformFall.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformFall : MonoBehaviour {
+    public float fallDelay = 1f;
+
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Awake () {
+
+        rb2d = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Invoke("Fall", fallDelay);
+        }
+    }
+
+    void Fall()
+    {
+        rb2d.isKinematic = false;
+    }
+}
