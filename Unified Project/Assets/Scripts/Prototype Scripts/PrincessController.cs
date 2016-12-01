f5a97d4cc88b8103415506f3dbb160292985f4b9
shelbyfirebaugh <slf404@nyu.edu>
2016-12-01 00:27:55 -0500

    added switching projectile types

--- Unified Project/Assets/Scripts/Prototype Scripts/PrincessController.cs
+++ Unified Project/Assets/Scripts/Prototype Scripts/PrincessController.cs
@@ -5,7 +5,9 @@ public class PrincessController : MonoBehaviour {
 
 	public float speed;
 
-	public GameObject shot;
+	public GameObject block;
+	public GameObject slow;
+	public GameObject knockdown;
 	public Transform shotSpawn;
 	public float fireRate;
 
@@ -23,7 +25,23 @@ public class PrincessController : MonoBehaviour {
 
 		if (Input.GetMouseButton (0) && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
-			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+			Instantiate (slow, shotSpawn.position, shotSpawn.rotation);
+		}
+
+		if (Input.GetMouseButton (1) && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Vector3 screenPoint = Input.mousePosition;
+			screenPoint.z = 20.0f; //distance of the plane from the camera
+			screenPoint = Camera.main.ScreenToWorldPoint(screenPoint);
+			Instantiate (block, screenPoint, shotSpawn.rotation);
+			Debug.Log("mouse: " + Camera.main.ScreenToWorldPoint(screenPoint));
+		}
+
+
+
+		if (Input.GetMouseButton (2) && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (knockdown, shotSpawn.position, shotSpawn.rotation);
 		}
 		
 
